ee04319cb28d4bcfc9a4f6e3ab5fecb6dc635674
soumil0507 <soumil0507@gmail.com>
2024-06-19 21:11:31 +0530

    inital commit

---
+++ src/test/java/bose/soumil/spring6di/Spring6diApplicationTests.java
@@ -0,0 +1,13 @@
+package bose.soumil.spring6di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6diApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
