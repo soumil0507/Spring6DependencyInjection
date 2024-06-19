01a95d84837d8c455b5ebce0f2aa0010c6a389c4
soumil0507 <soumil0507@gmail.com>
2024-06-19 23:24:55 +0530

    Created Primary Greeting Service Bean

---
+++ src/main/java/bose/soumil/spring6di/services/GreetingServicePrimary.java
@@ -0,0 +1,16 @@
+package bose.soumil.spring6di.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary
+@Service
+public class GreetingServicePrimary implements GreetingService{
+
+    @Override
+    public String sayGreeting() {
+        return "Hello from the Primary Bean!!";
+    }
+    
+
+}
